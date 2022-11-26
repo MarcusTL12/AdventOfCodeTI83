92c818e0b9e2278be63837a3bce798edd088d835
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-11-26 14:10:09 +0100

    Cleanup

--- 2015/day1/part1.asm
+++ 2015/day1/part1.asm
@@ -7,26 +7,26 @@ main:
     bcall(_clrscrf)
     bcall(_homeup)
 
-    ld bc,input
-    ld d, 0
-    ld e, 40
-    ld hl, 0
+    ld bc, input ; bc = &input
+    ld d, 0      ; d = 0
+    ld e, 40     ; e = 40
+    ld hl, 0     ; hl = 0
 
     loop1:
-        ld a, (bc) ; Move next char into a
-        inc bc ; bc += 1
-        cp d ; a == 0
-        jp z, loop1_exit
-        sub e ; a -= 40
-        jp z, inc_ans
-            dec hl
-            jp loop1
-        inc_ans:
-            inc hl
-            jp loop1
+        ld a, (bc)       ; a = *bc
+        inc bc           ; bc += 1
+        cp d             ; if a == 0
+        jp z, loop1_exit ; break loop1
+        cp e             ; if a != 40
+        jp z, inc_ans    ; {
+            dec hl       ;    hl -= 1
+            jp loop1     ; }
+        inc_ans:         ; else {
+            inc hl       ;    hl += 1
+            jp loop1     ; }
     loop1_exit:
 
-    bcall(_disphl)
+    bcall(_disphl) ; print(hl)
 
     bcall(_getkey) ; Pause
     ret
