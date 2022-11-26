319d2eaa2ef02e0b7e3095bdf61b948398d55919
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-11-26 23:00:36 +0100

    Seems like it might be working

--- util/mul_a_10.asm
+++ util/mul_a_10.asm
@@ -3,15 +3,15 @@
 
 ; multiplies a by 10
 ; destroys
-;   l
+;   d
 ; Time: 20 clk (+ call/ret)
 ; Space: 5 (+ call/ret)
 mul_a_10:
     add a, a
-    ld l, a
+    ld d, a
     add a, a
     add a, a
-    add a, l
+    add a, d
     ret
 
 #endif
--- util/parse_u8.asm
+++ util/parse_u8.asm
@@ -8,23 +8,25 @@
 ; if hl points to non-numerical string, a will be set to 0
 ; destroys:
 ;   bc
+;   de
 ;   hl (will be set to adress of first non-numeric character)
 parse_u8:
     ld b, 0
     ld a, (hl)
     sub 48
     cp 10
-    jp c, parse_u8_done
+    jp nc, parse_u8_done ; Check hl[0] is numeric
 
-    ld b, a
+    ld b, a ; if so, save to b
+
     inc hl
     ld a, (hl)
     sub 48
     cp 10
-    jp c, parse_u8_done
+    jp nc, parse_u8_done ; Check hl[1] is numeric
 
-    ld a, c
-    ld b, a
+    ld c, a ; If so, b = 10 * b + a
+    ld a, b
     call mul_a_10
     add a, c
     ld b, a
@@ -33,16 +35,15 @@ parse_u8:
     ld a, (hl)
     sub 48
     cp 10
-    jp c, parse_u8_done
+    jp nc, parse_u8_done ; Check hl[2] is numeric
 
-    ld a, c
-    ld b, a
+    ld c, a ; If so, b = 10 * b + a
+    ld a, b
     call mul_a_10
     add a, c
     ld b, a
 
     parse_u8_done:
-
     ld a, b
     ret
 
