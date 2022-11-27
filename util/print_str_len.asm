0ff6e23b70ce3175c464fd7710c234623271ab4b
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-11-27 11:57:14 +0100

    print str len

---
+++ util/print_str_len.asm
@@ -0,0 +1,15 @@
+#ifndef print_str_len_inc
+#define print_str_len_inc
+
+; print string of given length
+; hl: pointer to string
+; b: length of string
+; destroys a
+print_str_len:
+    ld a, (hl)
+    bcall(_putc)
+    inc hl
+    djnz print_str_len
+    ret
+
+#endif
