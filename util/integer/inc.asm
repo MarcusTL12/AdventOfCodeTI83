9834b1e14a0c138a55a2affb059f8cf8bfadf277
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-11-28 18:53:01 +0100

    moved inc

---
+++ util/integer/inc.asm
@@ -0,0 +1,15 @@
+#ifndef integer_inc_inc
+#define integer_inc_inc
+
+; Increments integer in memory. Goes on until non-carry,
+; so do not use for wrap-around.
+; hl: pointer to integer
+; destroys:
+;   hl: points to first byte to not be incremented
+integer_inc:
+    inc (hl)
+    inc hl
+    jp c, integer_inc
+    ret
+
+#endif
