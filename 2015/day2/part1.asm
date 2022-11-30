fba49b05eeba16abb3c310d0ef0d6d75a5fe9d18
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-11-30 01:04:02 +0100

    Would work if 8 bit was fine

--- 2015/day2/part1.asm
+++ 2015/day2/part1.asm
@@ -3,14 +3,18 @@
 title:
    .db "2015 d2p1",0
 
+#include "../../util/add_a2hl.asm"
+
+#include "../../util/debug/push_all.asm"
+
 main:
     bcall(_clrscrf)
     bcall(_homeup)
 
     ; defining variables
-    #define xyz saferam1
-    #define buf saferam1 + 3
-    #define ans saferam1 + 4
+    #define xyz     saferam1
+    #define buf     saferam1 + 3
+    #define ans     saferam1 + 4
     #define bcd_buf saferam1 + 8
 
     ; Init ans = 0
@@ -19,37 +23,79 @@ main:
     ld (ans + 2), hl
 
     ld hl, input
-    ld de, xyz
-    ld b, 3
-    call parse_line
 
-    ; buf = x * y
-    ld hl, (xyz)
-    bcall(_htimesl)
-    ld a, l
-    ld (buf), a
+    loop1:
+        ; parse next line
+        ld de, xyz
+        ld b, 3
+        call parse_line
+        push hl
 
-    ; l = x + y
-    ld hl, (xyz)
-    ld a, h
-    add a, l
-    ld l, a
+        ; buf = x * y
+        ld hl, (xyz)
+        bcall(_htimesl)
+        ld a, l
+        ld (buf), a
 
-    ; l = l * z
-    ld a, (xyz + 2)
-    ld h, a
-    bcall(_htimesl)
+        ; l = x + y
+        ld hl, (xyz)
+        ld a, h
+        add a, l
+        ld l, a
 
-    ; buf = (buf + l) * 2
-    ld a, (buf)
-    add a, l
-    sla a
-    ld (buf), a
+        ; l = l * z
+        ld a, (xyz + 2)
+        ld h, a
+        bcall(_htimesl)
 
-    ; ans += buf
-    ld a, (buf)
-    ld hl, ans
-    call integer_add_a
+        ; buf = (buf + l) * 2
+        ld a, (buf)
+        add a, l
+        add a, a
+        ld (buf), a
+
+        ; find index of largest side length
+        ld hl, xyz
+        ld b, 3
+        call max_u8
+
+        ; find indices of two smallest sides
+        ld a, d
+        add a, a
+        ld hl, min_two_dims_map
+        add_a2hl
+        ld d, (hl)
+        inc hl
+        ld e, (hl)
+
+        ; d = xyz[d]
+        ld hl, xyz
+        ld a, d
+        add_a2hl
+        ld a, (hl)
+        ld d, a
+
+        ; e = xyz[e]
+        ld hl, xyz
+        ld a, e
+        add_a2hl
+        ld a, (hl)
+        ld e, a
+
+        ; a = buf + d * e
+        ex de, hl
+        bcall(_htimesl)
+        ld a, (buf)
+        add a, l
+
+        ; ans += a
+        ld hl, ans
+        call integer_add_a
+
+        pop hl
+        xor a
+        cp (hl)
+        jp nz, loop1
 
     ; print ans
     ld hl, ans
@@ -86,7 +132,6 @@ parse_line:
     djnz parse_line
     ret
 
-
 #include "../../util/parse_u8.asm"
 #include "../../util/max_u8.asm"
 
@@ -100,8 +145,9 @@ min_two_dims_map:
     .db 0,2
     .db 0,1
 
-input:
-    .db "2x3x4",10,"1x1x10",10
-
 ; input:
-;     .db "1x1x10",10
+;     .db "2x3x4",10,"1x1x10",10,0
+
+input:
+    #incbin "input.txt"
+    .db 0
--- util/add_a2hl.asm
+++ util/add_a2hl.asm
@@ -2,13 +2,13 @@
 #define add_a2hl_inc
 
 ; Adds a to hl
-; time: 29/30
+; time: 19/20
 ; destroys:
 ;   a
-#define add_a2hl
-#defcont    add a,l
-#defcont    \ ld l,a
-#defcont    \ jr nc, $+3
-#defcont    \ inc h
+.define add_a2hl
+.defcont    add a,l
+.defcont    \ ld l,a
+.defcont    \ jr nc, $+3
+.defcont    \ inc h
 
 #endif
