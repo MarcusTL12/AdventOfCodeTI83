dafc6031dfb435ab461f5a54bad338d353ca1c1b
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-12-03 09:47:27 +0100

    more docs on qsort

--- util/qsort.asm
+++ util/qsort.asm
@@ -3,7 +3,23 @@
 
 ; quicksort (unstable, random)
 ; input
+; hl: pointer to array
+; de: length of array
+; b: size of element
+; saferam[0:1]: function pointer to comparison function:
+;   input:
+;   hl: pointer to element a
+;   de: pointer to element b
+;   compares a - b
+;   output: zero flag set if equal, sign flag set if b > a
 qsort:
+    ret
+
+; input
+; hl: pointer to beginning of array
+; de: pointer to end of array
+qsort_partition:
+    push hl
     ret
 
 #endif
